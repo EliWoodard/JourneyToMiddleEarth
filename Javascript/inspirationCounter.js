0f46832daebac674b537a05916a4a114164095ed
EliWoodard <elijahwoodard136@gmail.com>
2023-05-21 10:10:13 -0600

    Added Inpiration Counter

---
+++ Javascript/inspirationCounter.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const counterValue = document.querySelector(".counter-value");
+  const incrementBtn = document.querySelector(".increment");
+  const decrementBtn = document.querySelector(".decrement");
+
+  let counter = 0;
+
+  const updateCounterValue = () => {
+    counterValue.textContent = counter;
+  };
+
+  incrementBtn.addEventListener("click", () => {
+    if (counter < 4) {
+      counter++;
+      updateCounterValue();
+    }
+  });
+
+  decrementBtn.addEventListener("click", () => {
+    if (counter > 0) {
+      counter--;
+      updateCounterValue();
+    }
+  });
+});
