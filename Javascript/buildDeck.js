e8bbfdd3c20034bdc1391b1659b032a6e2d3a176
EliWoodard <elijahwoodard136@gmail.com>
2023-05-25 16:27:06 -0600

    Cards left Addition

--- Javascript/buildDeck.js
+++ Javascript/buildDeck.js
@@ -1,36 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('buildDeckButton').addEventListener('click', buildDeck);
-  });
-  
-  // The deckContainer array will store the user's selected cards.
-  let deckContainer = [];
-  
-  function buildDeck() {
-    // Clear the deckContainer array
-    deckContainer = [];
-  
-    // Iterate over all the card elements and check the number next to each card.
-    let cards = document.getElementsByClassName('card');
-  
-    for (let i = 0; i < cards.length; i++) {
+  document.getElementById('buildDeckButton').addEventListener('click', buildDeck);
+});
+
+let deckContainer = [];
+
+function buildDeck() {
+  deckContainer = [];
+
+  let cards = document.getElementsByClassName('card');
+
+  for (let i = 0; i < cards.length; i++) {
       let numberElement = cards[i].querySelector('.number');
       let cardCount = Number(numberElement.innerText);
-  
+
       if (cardCount > 0) {
-        // This card has been selected, so add it to the deck.
-  
-        // Get the card's details
-        let cardDetails = cards[i].querySelector('.cardImage img').alt;
-        let cardImage = cards[i].querySelector('.cardImage img').getAttribute('src');
-  
-        // Add the card to the deckContainer array for each selected
-        for(let j = 0; j < cardCount; j++) {
-          deckContainer.push({name: cardDetails, img: cardImage});
-        }
+          let cardDetails = cards[i].querySelector('.cardImage img').alt;
+          let cardImage = cards[i].querySelector('.cardImage img').getAttribute('src');
+
+          for(let j = 0; j < cardCount; j++) {
+              deckContainer.push({name: cardDetails, img: cardImage});
+          }
       }
-    }
-  
-    // Print the deckContainer array to the console to verify it was populated correctly
-    console.log(deckContainer);
   }
-  
+
+  let cardBack = document.querySelector('.cardBack');
+  let cardCountDisplay = document.querySelector('.cardCount');
+
+  if(deckContainer.length > 0) {
+      cardBack.style.display = 'flex';
+      cardCountDisplay.textContent = deckContainer.length;
+  } else {
+      cardBack.style.display = 'none';
+      cardCountDisplay.textContent = '';
+  }
+
+  console.log(deckContainer);
+}
