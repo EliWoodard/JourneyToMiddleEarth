a083c8bae00551f6db8aac70b3b70392a677f850
EliWoodard <elijahwoodard136@gmail.com>
2023-05-29 09:31:21 -0600

    Added Animations/Styles

--- Javascript/addDamage.js
+++ Javascript/addDamage.js
@@ -47,6 +47,17 @@ document.addEventListener('DOMContentLoaded', function() {
             damageSave.appendChild(newImageContainer);
             selectedDamage.push(damageBox);
             damageBox.classList.add('selectedDamage');
+
+            newImageContainer.addEventListener('mousemove', (ev) => {
+              Card3D(newImageContainer, ev);
+            });
+            
+            newImageContainer.addEventListener('mouseleave', (ev) => {
+              newFrontImage.style.transform = 'rotateX(0deg) rotateY(0deg)';
+              newFrontImage.style.filter = 'brightness(1)';
+              newBackImage.style.transform = 'rotateX(0deg) rotateY(0deg)';
+              newBackImage.style.filter = 'brightness(1)';
+            });
         } else if (selectedDamage.includes(damageBox)) {
             const imageToRemove = damageSave.querySelector(`div > img[src="${imageSrc}"]`);
             const containerToRemove = imageToRemove.parentNode;
@@ -57,6 +68,71 @@ document.addEventListener('DOMContentLoaded', function() {
             damageBox.classList.remove('selectedDamage');
         }
       });
+
+      document.addEventListener('mouseover', function(event) {
+        if(event.target.classList.contains('damageImageDisplay')) {
+            const parentDiv = event.target.parentElement;
+            throttle(() => Card3D(parentDiv, event), 100)();
+            event.target.classList.add('damageImageDisplayZoom');
+        }
     });
-  });
+    
+    document.addEventListener('mouseout', function(event) {
+        if(event.target.classList.contains('damageImageDisplay')) {
+            event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            event.target.style.filter = 'brightness(1)';
+            event.target.classList.remove('damageImageDisplayZoom');
+        }
+    });
+
+    function map(val, minA, maxA, minB, maxB) {
+      return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+    }
   
+    function Card3D(card, ev) {
+      let frontImage = card.querySelector('.front');
+      let backImage = card.querySelector('.back');
+      let img = frontImage.style.display !== 'none' ? frontImage : backImage;
+  
+      if (img) {
+        let cardRect = card.getBoundingClientRect();
+        let mouseX = ev.offsetX;
+        let mouseY = ev.offsetY;
+        let rotateY = map(mouseX, 0, cardRect.width, -25, 25);
+        let rotateX = map(mouseY, 0, cardRect.height, 25, -25);
+        let brightness = map(mouseY, 0, cardRect.height, 1.5, 0.5);
+  
+        img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        img.style.filter = `brightness(${brightness})`;
+      }
+    }
+  });
+
+  function throttle(func, wait) {
+    let timeout = null;
+    let remaining = 0;
+    let previous = 0;
+
+    let later = function() {
+        previous = Date.now();
+        timeout = null;
+        func.apply(null);
+    };
+
+    return function() {
+        let now = Date.now();
+        if (!previous) previous = now;
+        let remaining = wait - (now - previous);
+        if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            func.apply(null);
+        } else if (!timeout) {
+            timeout = setTimeout(later, remaining);
+        }
+    };
+  }
+});
