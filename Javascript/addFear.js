58b9ed6ae6e70e272099129d2093249a868391fe
EliWoodard <elijahwoodard136@gmail.com>
2023-05-28 22:38:41 -0600

    Added Fear Card Animation

--- Javascript/addFear.js
+++ Javascript/addFear.js
@@ -3,6 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
   const fearModal = document.querySelector('.fear');
   const fearSave = document.querySelector('.fearSave');
 
+  // Store fear boxes in a variable to prevent querying the DOM each time.
+  const fearBoxes = document.querySelectorAll('.fear-box');
+
+  // A simple throttle function to reduce the rate of function calls.
+  function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+      const args = arguments;
+      const context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(() => inThrottle = false, limit);
+      }
+    }
+  }
+
   addFearButton.addEventListener('click', function() {
     fearModal.style.display = 'block';
   });
@@ -14,8 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   const selectedFear = [];
-
-  const fearBoxes = document.querySelectorAll('.fear-box');
 
   fearBoxes.forEach(function(fearBox) {
     fearBox.addEventListener('click', function(event) {
@@ -47,6 +62,17 @@ document.addEventListener('DOMContentLoaded', function() {
           fearSave.appendChild(newImageContainer);
           selectedFear.push(fearBox);
           fearBox.classList.add('selectedFear');
+
+          newImageContainer.addEventListener('mousemove', (ev) => {
+            Card3D(newImageContainer, ev);
+          });
+          
+          newImageContainer.addEventListener('mouseleave', (ev) => {
+            newFrontImage.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            newFrontImage.style.filter = 'brightness(1)';
+            newBackImage.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            newBackImage.style.filter = 'brightness(1)';
+          });
       } else if (selectedFear.includes(fearBox)) {
           const imageToRemove = fearSave.querySelector(`div > img[src="${imageSrc}"]`);
           const containerToRemove = imageToRemove.parentNode;
@@ -57,5 +83,46 @@ document.addEventListener('DOMContentLoaded', function() {
           fearBox.classList.remove('selectedFear');
       }
     });
+  });
+
+  function map(val, minA, maxA, minB, maxB) {
+    return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+  }
+  
+  function Card3D(card, ev) {
+    let frontImage = card.querySelector('.front');
+    let backImage = card.querySelector('.back');
+  
+    if (!frontImage && !backImage) {
+      return;
+    }
+  
+    let img;
+    if(frontImage && frontImage.style.display !== 'none') {
+      img = frontImage;
+    } else if(backImage) {
+      img = backImage;
+    } else {
+      return;
+    }
+  
+    let cardRect = card.getBoundingClientRect();
+    let mouseX = ev.offsetX;
+    let mouseY = ev.offsetY;
+    let rotateY = map(mouseX, 0, cardRect.width, -25, 25);
+    let rotateX = map(mouseY, 0, cardRect.height, 25, -25);
+    let brightness = map(mouseY, 0, cardRect.height, 1.5, 0.5);
+  
+    img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    img.style.filter = `brightness(${brightness})`;
+  }
+  
+
+  document.addEventListener('mouseout', function(event) {
+    if(event.target.classList.contains('fearImageDisplay')) {
+      event.target.style.transform = 'rotateX(0deg) rotateY(0deg)';
+      event.target.style.filter = 'brightness(1)';
+      event.target.classList.remove('fearImageDisplayZoom');
+    }
   });
 });
