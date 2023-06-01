3097df8d07d68e2fbda8181d5805ef19890b43ed
EliWoodard <elijahwoodard136@gmail.com>
2023-06-01 11:55:13 -0600

    Added 3D Animation to Creature Cards

--- Javascript/addCreature.js
+++ Javascript/addCreature.js
@@ -3,6 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const creatureModal = document.querySelector('.creatureModal');
   const creatureSave = document.querySelector('.creatureSave');
 
+  function Card3D(card, ev) {
+    let img = card.querySelector('img');
+    if (img) {
+      let cardRect = card.getBoundingClientRect();
+      let mouseX = ev.clientX - cardRect.left;
+      let mouseY = ev.clientY - cardRect.top;
+      let rotateY = map(mouseX, 0, cardRect.width, -25, 25);
+      let rotateX = map(mouseY, 0, cardRect.height, 25, -25);
+      let brightness = map(mouseY, 0, cardRect.height, 1.5, 0.5);
+
+      img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      img.style.filter = `brightness(${brightness})`;
+    }
+  }
+
   addCreatureButton.addEventListener('click', function() {
     creatureModal.style.display = 'block';
   });
@@ -22,41 +37,65 @@ document.addEventListener('DOMContentLoaded', function() {
     const imageSrc = image.src;
 
     if (selectedCreatures.includes(imageSrc)) {
+      const newImageContainer = document.createElement('div');
+      newImageContainer.classList.add('creatureCardProp');
+
       const newImage = document.createElement('img');
       newImage.src = imageSrc;
-      newImage.style.width = '19%';
-      newImage.style.borderRadius = "5%";
+      newImage.classList.add('creatureImageDisplay');
 
-      creatureSave.appendChild(newImage);
+      newImageContainer.appendChild(newImage);
+      creatureSave.appendChild(newImageContainer);
 
       creatureBox.classList.add('selectedCreature');
+
+      newImageContainer.addEventListener('mousemove', function(event) {
+        Card3D(newImageContainer, event);
+      });
+
+      newImageContainer.addEventListener('mouseleave', function() {
+        newImage.style.transform = 'rotateX(0deg) rotateY(0deg)';
+        newImage.style.filter = 'brightness(1)';
+      });
     }
   });
 
-  creatureBoxes.forEach(function(creatureBox) {
-    creatureBox.addEventListener('click', function(event) {
+    creatureBoxes.forEach(function(creatureBox) {
+    creatureBox.addEventListener('click', function() {
       const image = creatureBox.querySelector('.creatureImages');
       const imageSrc = image.src;
 
       if (selectedCreatures.includes(imageSrc)) {
-        const imageToRemove = creatureSave.querySelector(`img[src="${imageSrc}"]`);
-        creatureSave.removeChild(imageToRemove);
+        const containerToRemove = creatureSave.querySelector(`.creatureCardProp > img[src="${imageSrc}"]`).parentNode;
+        creatureSave.removeChild(containerToRemove);
 
         const index = selectedCreatures.indexOf(imageSrc);
         selectedCreatures.splice(index, 1);
 
         creatureBox.classList.remove('selectedCreature');
       } else {
+        const newImageContainer = document.createElement('div');
+        newImageContainer.classList.add('creatureCardProp');
+
         const newImage = document.createElement('img');
         newImage.src = imageSrc;
-        newImage.style.width = '19%';
-        newImage.style.borderRadius = "5%";
+        newImage.classList.add('creatureImageDisplay');
 
-        creatureSave.appendChild(newImage);
+        newImageContainer.appendChild(newImage);
+        creatureSave.appendChild(newImageContainer);
 
         selectedCreatures.push(imageSrc);
 
         creatureBox.classList.add('selectedCreature');
+
+        newImageContainer.addEventListener('mousemove', function(event) {
+          Card3D(newImageContainer, event);
+        });
+
+        newImageContainer.addEventListener('mouseleave', function() {
+          newImage.style.transform = 'rotateX(0deg) rotateY(0deg)';
+          newImage.style.filter = 'brightness(1)';
+        });
       }
 
       localStorage.setItem('selectedCreatures', JSON.stringify(selectedCreatures));
