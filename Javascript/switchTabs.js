b689a27af8d89c52789816aa4fc8048d54a26845
EliWoodard <elijahwoodard136@gmail.com>
2023-06-11 12:24:38 -0600

    Added Button Effect

--- Javascript/switchTabs.js
+++ Javascript/switchTabs.js
@@ -5,7 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidePanel = document.querySelector(".sidePanel");
 
   tabs.forEach((tab) => {
-    tab.addEventListener("click", function () {
+    tab.addEventListener("click", function (e) {
+      // Handle Ripple
+      createRipple.call(this, e);
+
+      // Existing Tab Functionality
       tabs.forEach((item) => item.classList.remove("active"));
       this.classList.add("active");
       const innerTextLower = this.innerText.toLowerCase();
@@ -25,3 +29,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+function createRipple(e) {
+  if (this.getElementsByClassName('ripple').length > 0) {
+    this.removeChild(this.childNodes[1]);
+  }
+
+  var circle = document.createElement('div');
+  this.appendChild(circle);
+
+  var d = Math.max(this.clientWidth, this.clientHeight);
+  circle.style.width = circle.style.height = d + 'px';
+
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
+  circle.style.top = e.clientY - this.offsetTop - d / 2 + 'px';
+
+  circle.classList.add('ripple');
+}
