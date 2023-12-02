20288a0a16915eebe628c43d14fa74734f9a8024
EliWoodard <elijahwoodard136@gmail.com>
2023-12-02 13:28:07 -0700

    Added In Character Cards

---
+++ Javascript/characterColumn.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('#choosablePlayers');
+
+        for (let i = 1; i < 36; i += 2) {
+            const imageDiv = document.createElement('div');
+            imageDiv.classList.add('characterCard');
+
+            const img = document.createElement('img');
+            img.classList.add('characterImage');
+            img.src = '../Images/CharacterCards/' + i +'.jpg';
+            img.alt = 'Image ' + i;
+
+            imageDiv.appendChild(img);
+            container.appendChild(imageDiv);
+        }
+});
